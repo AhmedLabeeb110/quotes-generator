d9a5fd7c9a7b970419e7508f929c1dca3fec715b
Ahmed Labeeb <labeeb.edu123@gmail.com>
2023-03-09 05:45:10 +0600

    Quotes Generator

---
+++ script.js
@@ -0,0 +1,36 @@
+// Variables
+
+let btn = document.querySelector("#new-quote");
+let quote = document.querySelector(".quote");
+let person = document.querySelector(".person");
+
+const quotes = [
+    {
+        quote: `You can get everything in life you want if you will just help enough other people get what they want.`,
+        person: `Zig Ziglar`
+    }, 
+    {
+        quote: `Inspiration does exist, but it must find you working.`,
+        person: `Pablo Picasso`
+    },
+    {
+        quote: `Don't settle for average. Bring your best to the moment. Then, whether it fails or succeeds, at least you know you gave all you had.`,
+        person: `Angela Bassett`
+    },
+    {
+        quote: `Show up, show up, show up, and after a while the muse shows up, too.`,
+        person: `Isabel Allende`
+    }, 
+    {
+        quote: `Don't bunt. Aim out of the ballpark. Aim for the company of immortals.`,
+        person: `David Ogilvy`,
+    },
+    {
+        quote: `I have stood on a mountain of no’s for one yes.`,
+        person: `Barbara Elaine Smith`,
+    },
+    {
+        quote: `If you believe something needs to exist, if it's something you want to use yourself, don't let anyone ever stop you from doing it.`,
+        person: `obias Lütke`
+    }
+]
